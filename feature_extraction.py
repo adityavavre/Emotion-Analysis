41e36091544582def2036887cd6e469972ba14dd
adityavave <aditya.vavre@gmail.com>
2020-08-10 16:55:55 +0530

    Added glove word embedding feature extraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,4 +1,33 @@
+from typing import List
+import numpy as np
+from nltk.tokenize import word_tokenize
+
+def get_glove(sentences: List):
+    embeddings_dict = {}
+    embedding_dim = 50
+    with open("glove.6B.50d.txt", 'r') as f:
+        for line in f:
+            values = line.split()
+            print(values)
+            exit(0)
+            word = values[0]
+            vector = np.asarray(values[1:], dtype='float32')
+            embeddings_dict[word] = vector
+
+    # all_words = []
+    feature = []
+    for sent in sentences:
+        tokenize_word = word_tokenize(sent)
+        vec = np.array([0]*embedding_dim, dtype='float32')
+        for word in tokenize_word:
+            # all_words.append(word)
+            vec += embeddings_dict.get(word)
+        vec /= len(tokenize_word)
+        feature.append(vec)
+    return feature
+
+def extract_features(sentences: List):
+    features = []
 
 
-def extract_features():
-    pass
+    return features
