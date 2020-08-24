7426903f85f4fc7013cd03ee6887c470c99b66f0
adityavave <aditya.vavre@gmail.com>
2020-08-24 22:53:01 +0530

    Added code to scrap data from different sub reddits

--- reddit_scraper/reddit_scraper/spiders/deep_learner.py
+++ reddit_scraper/reddit_scraper/spiders/deep_learner.py
@@ -4,17 +4,19 @@ import scrapy
 class DeepLearnerSpider(scrapy.Spider):
     name = 'deep_learner'
     allowed_domains = ['www.reddit.com']
-    start_urls = ['http://reddit.com/r/deeplearning/']
+    # start_urls = ['http://reddit.com/r/deeplearning/', 'http://reddit.com/r/education/', 'http://reddit.com/r/india/']
+    # start_urls = ['http://reddit.com/r/education/']
+    start_urls = ['http://reddit.com/r/india/']
 
-    custom_settings = {
-        'DEPTH_LIMIT': 20
-    }
+    # custom_settings = {
+    #     'DEPTH_LIMIT': 128
+    # }
 
     def start_requests(self):
         self.index = 0
         for url in self.start_urls:
             # We make a request to each url and call the parse function on the http response.
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse, meta={"reset_depth": True, "depth_max": 128})
 
     def parse(self, response, **kwargs):
         # filename = "deep_learning_response" + str(self.index) + ".txt"
@@ -23,7 +25,7 @@ class DeepLearnerSpider(scrapy.Spider):
         #     f.write(response.body)
         # self.log('Saved file %s' % filename)
         post = response.xpath('//*[@class="_1qeIAgB0cPwnLhDF9XSiJM"]/text()').extract()
-        yield {"Post": set(post)}
+        yield {"Post": post}
 
         next_page = response.xpath('//link[@rel="next"]/@href').extract_first()
         if next_page:
